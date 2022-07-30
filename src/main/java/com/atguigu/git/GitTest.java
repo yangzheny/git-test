6f68d927c5676c60356b0c3e9be08ff95ae694cb
yzrain <yzrain@qq.com>
2022-07-30 11:29:55 +0800

    master commit2 idea

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git 3");
         System.out.println("hello git 4");
         System.out.println("hello git 5");
+        System.out.println("master test");
     }
 }
