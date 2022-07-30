38d2968edca0d078e72b52887f38200c9f2e82c3
yzrain <yzrain@qq.com>
2022-07-30 11:07:49 +0800

    first commit idea

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author yzrain
+ * @version 1.0
+ * @date 2022/7/30 11:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git 2");
+    }
+}
